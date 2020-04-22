52b5717a61343d493a332c0b4345119a3580aa67
Eric Beecroft <ebeecroft@mail.csuchico.edu>
2020-04-21 18:48:03 -0700

    Updated donation code

--- Resources/Code/dbox/boxrate.cpp
+++ Resources/Code/dbox/boxrate.cpp
@@ -1,26 +1,25 @@
 #include <stdlib.h>
 #include <iostream>
-double getTax(int& pointsDonated, double taxbase, double taxincrement);
+double getTax(int& pointsDonated, double taxincrement);
 int main(int argc, char* argv[])
 {
-   if(argc == 4)
+   if(argc == 3)
    {
       //Setup up of donation variables
       int count = 1, limit = 1;
       int pointsTaxed = 0;
-      double tax = 0.0, taxbase = 0.0, taxincrement = 0.0;
+      double tax = 0.0, taxincrement = 0.0;
 
       //Initializes the points variable
       for(int index = 0; index < limit; index++)
       {
          pointsTaxed = atoi(argv[count]);
-         taxbase = atof(argv[2]);
-         taxincrement = atof(argv[3]);
+         taxincrement = atof(argv[2]);
          count++;
       }
 
       //Retrieves donation points and returns them to ruby
-      tax = getTax(pointsTaxed, taxbase, taxincrement);
+      tax = getTax(pointsTaxed, taxincrement);
       std::cout << pointsTaxed << "," << tax << std::endl;
    }
 }
--- Resources/Code/dbox/tax.cpp
+++ Resources/Code/dbox/tax.cpp
@@ -1,23 +1,25 @@
 #include <math.h>
-double taxCalculation(int pointsDonated, double taxbase, double taxincrement);
+double taxCalculation(int pointsDonated, double taxincrement);
 
-double getTax(int& pointsDonated, double taxbase, double taxincrement)
+double getTax(int& pointsDonated, double taxincrement)
 {
    double tax = 0.0;
-   tax = taxCalculation(pointsDonated, taxbase, taxincrement);
+   tax = taxCalculation(pointsDonated, taxincrement);
    pointsDonated = round(pointsDonated * tax);
    return tax;
 }
 
-double taxCalculation(int pointsDonated, double taxbase, double taxincrement)
+double taxCalculation(int pointsDonated, double taxincrement)
 {
    //Sets up the variable data
-   int pointsbase = 10000;
+   int pointsbase = 5000;
+   int range = 200;
    double tax = 0.0;
+   double taxbase = 0.0;
    bool rateFound = false;
 
    //Performs the calculations
-   for(int i = 0; i < 100; i++)
+   for(int i = 0; i < range; i++)
    {
       //Identifies the tax bracket the user is in
       int taxbracket = (pointsbase + (pointsbase * i));
@@ -33,7 +35,7 @@ double taxCalculation(int pointsDonated, double taxbase, double taxincrement)
    //Sets the tax rate to the highest setting
    if(!rateFound)
    {
-      tax = taxbase * 100;
+      tax = taxincrement * range;
    }
    return tax;
 }
